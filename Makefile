e820158a65d4361c1a2caad32ece1b3c9fe6f12a
Stephan Renaud Pierre Tinel <tinel@ic1-ubt22-5004.intranet.epfl.ch>
2026-03-05 10:45:21 +0100

    P2 modularisation

--- Makefile
+++ Makefile
@@ -1,1 +1,20 @@
+CXX = g++
+CC  = $(CXX)
 
+CXXFLAGS = -std=c++11  # C++11, ou autre suivant vos préférences
+
+# Partie commentée : choisissez les options que vous voulez avoir
+#                    en décommentant la/les lignes correspondantes
+#
+CXXFLAGS += -pedantic -Wall         # pour les purs et durs
+# CXXFLAGS += -fsanitize=address    # pour analyse dynamique du code
+# CXXFLAGS += -g                    # pour debugger
+# CXXFLAGS += -O2                   # pour optimiser la vitesse
+
+all: Vecteur/testVecteur
+
+Vecteur.o: Vecteur/Vecteur.cc Vecteur/Vecteur.h
+
+testVecteur.o: Vecteur/testVecteur.cc Vecteur/Vecteur.h
+
+testVecteur: Vecteur/testVecteur.o Vecteur/Vecteur.o
--- Vecteur/testVecteur.cc
+++ Vecteur/testVecteur.cc
@@ -1,93 +1,7 @@
 #include <iostream>
-#include <cmath>
+#include "Vecteur.h"
 
-class Vecteur3D {
-	public:
-		// Sets
-		void set_comp(double const& x, double const& y, double const& z) {
-			comp_x=x;
-			comp_y=y;
-			comp_z=z;
-		}
-		
-		// Gets
-		double get_comp_x() const {
-			return comp_x;
-		}
-		double get_comp_y() const {
-			return comp_y;
-		}
-		double get_comp_z() const {
-			return comp_z;
-		}
-		
-		// Affiche
-		void affiche() const {
-			std::cout << comp_x << " " << comp_y << " " << comp_z << " ";
-		}
-		
-		// Arithmetique de vecteurs
-		bool compare(Vecteur3D const& autreVecteur, double const& marge=1e-10) const {
-			if ( (std::abs(autreVecteur.get_comp_x() - comp_x) > marge) or (std::abs(autreVecteur.get_comp_y() - comp_y) > marge) or (std::abs(autreVecteur.get_comp_z() - comp_z) > marge)) {
-				return false;
-			}
-			return true;
-		}
-		
-		Vecteur3D addition(Vecteur3D const& autreVecteur) const {
-			Vecteur3D resultatVecteur;
-			resultatVecteur.set_comp(comp_x+autreVecteur.get_comp_x(), comp_y+autreVecteur.get_comp_y(), comp_z+autreVecteur.get_comp_z());
-			return resultatVecteur;
-		}
-		
-		Vecteur3D soustraction(Vecteur3D const& autreVecteur) const {
-			Vecteur3D resultatVecteur;
-			resultatVecteur.set_comp(comp_x-autreVecteur.get_comp_x(), comp_y-autreVecteur.get_comp_y(), comp_z-autreVecteur.get_comp_z());
-			return resultatVecteur;
-		}
-		
-		Vecteur3D oppose() const {
-			Vecteur3D resultatVecteur;
-			resultatVecteur.set_comp(comp_x*(-1), comp_y*(-1), comp_z*(-1));
-			return resultatVecteur;
-		}
-		
-		Vecteur3D mult(double const& s) const {
-			Vecteur3D resultatVecteur;
-			resultatVecteur.set_comp(comp_x*s, comp_y*s, comp_z*s);
-			return resultatVecteur;
-		}
-		
-		double prod_scal(Vecteur3D const& autreVecteur) const {
-			return comp_x*autreVecteur.get_comp_x()+comp_y*autreVecteur.get_comp_y()+comp_z*autreVecteur.get_comp_z();
-		}
-		
-		Vecteur3D prod_vect(Vecteur3D const& autreVecteur) const {
-			Vecteur3D resultatVecteur;
-			resultatVecteur.set_comp(comp_y*autreVecteur.get_comp_z() - comp_z*autreVecteur.get_comp_y(), comp_z*autreVecteur.get_comp_x() - comp_x*autreVecteur.get_comp_z(), comp_x*autreVecteur.get_comp_y() - comp_y*autreVecteur.get_comp_x());
-			return resultatVecteur;
-		}
-		
-		double norme2() const {
-			return std::pow(comp_x, 2)+std::pow(comp_y, 2)+std::pow(comp_z, 2);
-		}
-		
-		double norme() const {
-			return std::sqrt(norme2());
-		}
-		
-		Vecteur3D unitaire() const {
-			Vecteur3D resultatVecteur;
-			double n=norme();
-			resultatVecteur.set_comp(comp_x/n, comp_y/n, comp_z/n);
-			return resultatVecteur;
-		}
-		
-	private:
-		double comp_x;
-		double comp_y;
-		double comp_z;
-};
+using namespace std;
 
 int main() {
 	Vecteur3D vecteur1;
@@ -96,13 +10,13 @@ int main() {
 	vecteur1.set_comp(2, 3, 4);
 	vecteur2.set_comp(3, 3, 4);
 	
-	std::cout << vecteur1.norme() << std::endl;
-	std::cout << vecteur1.norme2() << std::endl;
-	std::cout << vecteur2.norme() << std::endl;
+	cout << vecteur1.norme() << endl;
+	cout << vecteur1.norme2() << endl;
+	cout << vecteur2.norme() << endl;
 	
 	if (vecteur1.compare(vecteur2, 1)) {
-		std::cout << "Same vector" << std::endl;
-	} else { std::cout << "Diff vector" << std::endl; }
+		cout << "Same vector" << endl;
+	} else { cout << "Diff vector" << endl; }
 	
 	Vecteur3D vecteur3;
 	vecteur3 = vecteur1.addition(vecteur2);
