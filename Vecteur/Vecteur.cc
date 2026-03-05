026e5d00a0aabf9b41725dea78f2c9ac7052f1b9
Elias Helfand <elfand@tsf-428-wpa-5-009.epfl.ch>
2026-03-05 09:59:13 +0100

    Modified vecteur (removed struct)

--- Vecteur/Vecteur.cc
+++ Vecteur/Vecteur.cc
@@ -1,76 +1,75 @@
 #include <iostream>
 #include <cmath>
 
-struct Comp {
-	double x;
-	double y;
-	double z;
-};
-
 class Vecteur3D {
 	public:
+		// Sets
 		void set_comp(double const& x, double const& y, double const& z) {
-			comp.x=x;
-			comp.y=y;
-			comp.z=z;
+			comp_x=x;
+			comp_y=y;
+			comp_z=z;
 		}
 		
-		Comp get_comp() const {
-			return comp;
+		// Gets
+		double get_comp_x() const {
+			return comp_x;
+		}
+		double get_comp_y() const {
+			return comp_y;
+		}
+		double get_comp_z() const {
+			return comp_z;
 		}
 		
+		// Affiche
 		void affiche() const {
-			std::cout << comp.x << " " << comp.y << " " << comp.z << " ";
+			std::cout << comp_x << " " << comp_y << " " << comp_z << " ";
 		}
 		
+		// Arithmetique de vecteurs
 		bool compare(Vecteur3D const& autreVecteur, double const& marge=1e-10) const {
-			if ( (std::abs(autreVecteur.get_comp().x - comp.x) > marge) or (std::abs(autreVecteur.get_comp().y - comp.y) > marge) or (std::abs(autreVecteur.get_comp().z - comp.z) > marge)) {
+			if ( (std::abs(autreVecteur.get_comp_x() - comp_x) > marge) or (std::abs(autreVecteur.get_comp_y() - comp_y) > marge) or (std::abs(autreVecteur.get_comp_z() - comp_z) > marge)) {
 				return false;
 			}
 			return true;
 		}
 		
-		// Arithmetique de vecteurs
 		Vecteur3D addition(Vecteur3D const& autreVecteur) const {
 			Vecteur3D resultatVecteur;
-			resultatVecteur.set_comp(comp.x+autreVecteur.comp.x, comp.y+autreVecteur.comp.y, comp.z+autreVecteur.comp.z);
+			resultatVecteur.set_comp(comp_x+autreVecteur.get_comp_x(), comp_y+autreVecteur.get_comp_y(), comp_z+autreVecteur.get_comp_z());
 			return resultatVecteur;
 		}
 		
 		Vecteur3D soustraction(Vecteur3D const& autreVecteur) const {
 			Vecteur3D resultatVecteur;
-			resultatVecteur.set_comp(comp.x-autreVecteur.comp.x, comp.y-autreVecteur.comp.y, comp.z-autreVecteur.comp.z);
+			resultatVecteur.set_comp(comp_x-autreVecteur.get_comp_x(), comp_y-autreVecteur.get_comp_y(), comp_z-autreVecteur.get_comp_z());
 			return resultatVecteur;
 		}
 		
 		Vecteur3D oppose() const {
 			Vecteur3D resultatVecteur;
-			resultatVecteur.set_comp(comp.x*(-1), comp.y*(-1), comp.z*(-1));
+			resultatVecteur.set_comp(comp_x*(-1), comp_y*(-1), comp_z*(-1));
 			return resultatVecteur;
 		}
 		
 		Vecteur3D mult(double const& s) const {
 			Vecteur3D resultatVecteur;
-			resultatVecteur.set_comp(comp.x*s, comp.y*s, comp.z*s);
+			resultatVecteur.set_comp(comp_x*s, comp_y*s, comp_z*s);
 			return resultatVecteur;
 		}
 		
 		double prod_scal(Vecteur3D const& autreVecteur) const {
-			return comp.x*autreVecteur.comp.x+comp.y*autreVecteur.comp.y+comp.z*autreVecteur.comp.z;
+			return comp_x*autreVecteur.get_comp_x()+comp_y*autreVecteur.get_comp_y()+comp_z*autreVecteur.get_comp_z();
 		}
 		
 		Vecteur3D prod_vect(Vecteur3D const& autreVecteur) const {
-			Comp resultatComp;
-			resultatComp.x = comp.y*autreVecteur.comp.z - comp.z*autreVecteur.comp.y;
-			resultatComp.y = comp.z*autreVecteur.comp.x - comp.x*autreVecteur.comp.z;
-			resultatComp.z = comp.x*autreVecteur.comp.y - comp.y*autreVecteur.comp.x;
 			Vecteur3D resultatVecteur;
-			resultatVecteur.set_comp(resultatComp.x, resultatComp.y, resultatComp.z);
+			resultatVecteur.set_comp(comp_y*autreVecteur.get_comp_z() - comp_z*autreVecteur.get_comp_y(), comp_z*autreVecteur.get_comp_x() - comp_x*autreVecteur.get_comp_z(), comp_x*autreVecteur.get_comp_y() - comp_y*autreVecteur.get_comp_x());
 			return resultatVecteur;
 		}
 		
 		double norme2() const {
-			return std::pow(comp.x, 2)+std::pow(comp.y, 2)+std::pow(comp.z, 2);
+			return std::pow(comp_x, 2)+std::pow(comp_y, 2)+std::pow(comp_z, 2);
 		}
 		
 		double norme() const {
@@ -80,10 +79,12 @@ class Vecteur3D {
 		Vecteur3D unitaire() const {
 			Vecteur3D resultatVecteur;
 			double n=norme();
-			resultatVecteur.set_comp(comp.x/n, comp.y/n, comp.z/n);
+			resultatVecteur.set_comp(comp_x/n, comp_y/n, comp_z/n);
 			return resultatVecteur;
 		}
 		
 	private:
-		Comp comp;
+		double comp_x;
+		double comp_y;
+		double comp_z;
 };
